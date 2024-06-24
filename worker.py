b04c2f29b81b8bb100bc3acc41845b30dd0b6cdb
dream-well <dreamwell506@outlook.com>
2024-06-23 23:15:36 -0400

    fixed worker

--- worker.py
+++ worker.py
@@ -1,29 +1,24 @@
-from typing import Dict, Optional
-import bittensor as bt
-import time
-import json
-import os
-import uuid
-import ezkl
 import asyncio
-from fastapi import FastAPI
-from pydantic import BaseModel
-import traceback
+import json
 import multiprocessing
+import os
+import time
+import traceback
+from typing import Dict, Optional
+import uuid
 from itertools import chain
-import model0
 
+import bittensor as bt
+import ezkl
+from pydantic import BaseModel
+
+from fastapi import FastAPI
 app = FastAPI()
-
-class QueryZkProof(BaseModel):
-    query_input: Optional[Dict] = None
-    query_output: Optional[str] = None
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-
 def gen_witness(input_path, circuit_path, witness_path, vk_path, srs_path):
-    bt.logging.debug("Generating witness")
+    # bt.logging.debug("Generating witness")
     res = ezkl.gen_witness(
         input_path,
         circuit_path,
@@ -31,15 +26,18 @@ def gen_witness(input_path, circuit_path, witness_path, vk_path, srs_path):
         vk_path,
         srs_path,
     )
-    bt.logging.debug(f"Gen witness result: {res}")
+    # bt.logging.debug(f"Gen witness result: {res}")
 
 
 async def gen_proof(
     input_path, vk_path, witness_path, circuit_path, pk_path, proof_path, srs_path
 ):
+    start = time.time()
     gen_witness(input_path, circuit_path, witness_path, vk_path, srs_path)
+    print(f"gen witness took {time.time() - start}")
+    start = time.time()
 
-    bt.logging.debug("Generating proof")
+    # bt.logging.debug("Generating proof")
     res = ezkl.prove(
         witness_path,
         circuit_path,
@@ -48,17 +46,28 @@ async def gen_proof(
         "single",
         srs_path,
     )
-    bt.logging.debug(f"Proof generated: {proof_path}, result: {res}")
+    # bt.logging.debug(f"Proof generated: {proof_path}, result: {res} took {time.time() - start}")
 
 
-def proof_worker(
+async def proof_worker(
     input_path, vk_path, witness_path, circuit_path, pk_path, proof_path, srs_path
 ):
-    loop = asyncio.new_event_loop()
-    asyncio.set_event_loop(loop)
+    # loop = asyncio.new_event_loop()
+    # asyncio.set_event_loop(loop)
     try:
-        result = loop.run_until_complete(
-            gen_proof(
+        start = time.time()
+        # result = loop.run_until_complete(
+        #     gen_proof(
+        #         input_path,
+        #         vk_path,
+        #         witness_path,
+        #         circuit_path,
+        #         pk_path,
+        #         proof_path,
+        #         srs_path,
+        #     )
+        # )
+        result = await gen_proof(
                 input_path,
                 vk_path,
                 witness_path,
@@ -67,21 +76,26 @@ def proof_worker(
                 proof_path,
                 srs_path,
             )
-        )
+        print("Proof generation took", time.time() - start)
         return result
     finally:
-        loop.close()
+        # loop.close()
+        pass
 
 
 class VerifiedModelSession:
 
-    def __init__(self, model_id=None):
-        public_inputs = []
-        self.model_id = model_id
+    def __init__(self, public_inputs=None, model_id=None):
+        if public_inputs is None:
+            public_inputs = []
+        if model_id is None:
+            model_id = [0]
+        self.model_id = model_id[0]
         self.session_id = str(uuid.uuid4())
-
-        model_path = os.path.join(dir_path, f"model/model_{self.model_id}")
         self.model_name = f"model_{self.model_id}"
+        model_path = os.path.join(
+            dir_path, "model", self.model_name
+        )
 
         self.pk_path = os.path.join(model_path, "pk.key")
         self.vk_path = os.path.join(model_path, "vk.key")
@@ -90,9 +104,15 @@ class VerifiedModelSession:
         self.settings_path = os.path.join(model_path, "settings.json")
         self.sample_input_path = os.path.join(model_path, "input.json")
 
-        self.witness_path = os.path.join(dir_path, f"temp/witness_{self.model_id}_{self.session_id}.json")
-        self.input_path = os.path.join(dir_path, f"temp/input_{self.model_id}_{self.session_id}.json")
-        self.proof_path = os.path.join(dir_path, f"temp/proof_{self.model_name}_{self.session_id}.json")
+        self.witness_path = os.path.join(
+            dir_path, "temp", f"witness_{self.model_name}_{self.session_id}.json"
+        )
+        self.input_path = os.path.join(
+            dir_path, "temp", f"input_{self.model_name}_{self.session_id}.json"
+        )
+        self.proof_path = os.path.join(
+            dir_path, "temp", f"proof_{self.model_name}_{self.session_id}.json"
+        )
 
         self.public_inputs = public_inputs
 
@@ -112,7 +132,7 @@ class VerifiedModelSession:
 
         with open(self.input_path, "w", encoding="utf-8") as f:
             json.dump(data, f)
-        bt.logging.info(f"Input data: {data}")
+        # bt.logging.info(f"Input data: {data}")
 
     def gen_proof_file(self, proof_string, inputs):
         dir_name = os.path.dirname(self.proof_path)
@@ -148,9 +168,9 @@ class VerifiedModelSession:
                 with open(proof_path, "w", encoding="utf-8") as f:
                     f.write(proof)
                 proof_paths.append(proof_path)
-            bt.logging.debug(
-                f"Generating aggregated proof given proof paths: {proof_paths}"
-            )
+            # bt.logging.debug(
+                # f"Generating aggregated proof given proof paths: {proof_paths}"
+            # )
             path = os.path.join(
                 dir_path,
                 "temp",
@@ -159,7 +179,7 @@ class VerifiedModelSession:
             time_start = time.time()
             ezkl.aggregate(aggregation_snarks=proof_paths, output_path=path)
             time_delta = time.time() - time_start
-            bt.logging.info(f"Proof aggregation took {time_delta} seconds")
+            # bt.logging.info(f"Proof aggregation took {time_delta} seconds")
             with open(path, "r", encoding="utf-8") as f:
                 aggregated_proof = f.read()
             return aggregated_proof, time_delta
@@ -168,42 +188,43 @@ class VerifiedModelSession:
             traceback.print_exc()
             raise
 
-    def gen_proof(self, public_inputs):
-        self.public_inputs = public_inputs
+    async def gen_proof(self):
         try:
-            bt.logging.debug("Generating input file")
+            # bt.logging.debug("Generating input file")
             self.gen_input_file()
-            bt.logging.debug("Starting generating proof process...")
+            # bt.logging.debug("Starting generating proof process...")
             start_time = time.time()
-            with multiprocessing.Pool(1) as p:
-                bt.logging.debug(
-                    f"Starting proof generation with paths: {self.input_path}, {self.vk_path}, "
-                    f"{self.witness_path}, {self.circuit_path}, {self.pk_path}, {self.proof_path}, "
-                    f"{self.srs_path}"
-                )
-                p.apply(
-                    func=proof_worker,
-                    kwds={
-                        "input_path": self.input_path,
-                        "vk_path": self.vk_path,
-                        "witness_path": self.witness_path,
-                        "circuit_path": self.circuit_path,
-                        "pk_path": self.pk_path,
-                        "proof_path": self.proof_path,
-                        "srs_path": self.srs_path,
-                    },
-                )
-
+            # with multiprocessing.Pool(1) as p:
+            #     # bt.logging.debug(
+            #     #     f"Starting proof generation with paths: {self.input_path}, {self.vk_path}, "
+            #     #     f"{self.witness_path}, {self.circuit_path}, {self.pk_path}, {self.proof_path}, "
+            #     #     f"{self.srs_path}"
+            #     # )
+            #     p.apply(
+            #         func=proof_worker,
+            #         kwds={
+            #             "input_path": self.input_path,
+            #             "vk_path": self.vk_path,
+            #             "witness_path": self.witness_path,
+            #             "circuit_path": self.circuit_path,
+            #             "pk_path": self.pk_path,
+            #             "proof_path": self.proof_path,
+            #             "srs_path": self.srs_path,
+            #         },
+            #     )
+            await proof_worker(
+                self.input_path, self.vk_path, self.witness_path, self.circuit_path, self.pk_path, self.proof_path, self.srs_path
+            )
             end_time = time.time()
             proof_time = end_time - start_time
-            bt.logging.info(f"Proof generation took {proof_time} seconds")
+            # bt.logging.info(f"Proof generation took {proof_time} seconds")
             with open(self.proof_path, "r", encoding="utf-8") as f:
                 proof_content = f.read()
 
             return proof_content, proof_time
 
         except Exception as e:
-            bt.logging.error(f"An error occurred during proof generation: {e}")
+            print(f"An error occurred during proof generation: {e}")
             traceback.print_exc()
             raise
 
@@ -249,56 +270,54 @@ class VerifiedModelSession:
             os.remove(self.proof_path)
 
     def end(self):
-        self.remove_temp_files()
+        # self.remove_temp_files()
+        pass
 
     def __exit__(self, exc_type, exc_val, exc_tb):
 
         return None
 
-sessions = {
-    "0a92bc32ea02abe54159da70aeb541d52c3cba27c8708669eda634e096a86f8b": VerifiedModelSession("0a92bc32ea02abe54159da70aeb541d52c3cba27c8708669eda634e096a86f8b")
-}
+
+class QueryZkProof(BaseModel):
+    query_input: Optional[Dict] = None
+    query_output: Optional[str] = None
 
 @app.post("/generate_proof")
-def generateZkProof(synapse: QueryZkProof) -> QueryZkProof:
-    query_input = synapse.query_input
+async def generateZkProof(synapse: QueryZkProof) -> QueryZkProof:
     time_in = time.time()
-    bt.logging.info("Received request from validator")
-    bt.logging.info(f"Input data: {query_input}")
-    if query_input is not None:
-        public_inputs = query_input["public_inputs"]
-    else:
+    # bt.logging.debug("Received request from validator")
+    print(f"Input data: {synapse.query_input} \n")
+
+    if not synapse.query_input or not synapse.query_input.get(
+        "public_inputs", None
+    ):
         bt.logging.error("Received empty query input")
+        synapse.query_output = "Empty query input"
         return synapse
 
+    model_id = synapse.query_input.get("model_id", [0])
+    public_inputs = synapse.query_input["public_inputs"]
+    if model_id == [0]:
+        public_inputs = [public_inputs]
+
+    # Run inputs through the model and generate a proof.
     try:
-        bt.logging.info(f"Public inputs: {public_inputs}")
-        model_id = str(query_input['model_id'][0])
-        if model_id == "0":
-            return model0.generateModel0Proof(synapse = synapse)
-        model = sessions.get(model_id)
-        query_output, proof_time = model.gen_proof(public_inputs)
-        model.end()
+        model_session = VerifiedModelSession(public_inputs, model_id)
+        # bt.logging.debug("Model session created successfully")
+        synapse.query_output, proof_time = await model_session.gen_proof()
+        # model_session.end()
     except Exception as e:
         synapse.query_output = "An error occurred"
-        bt.logging.error("An error occurred while generating proven output", e)
-        return synapse
-    synapse.query_output = query_output
-    bt.logging.info(f"✅ Proof completed {query_output}")
+        bt.logging.error(f"An error occurred while generating proven output\n{e}")
+        proof_time = time.time() - time_in
+
     time_out = time.time()
     delta_t = time_out - time_in
-    bt.logging.info(
-        f"Total response time {delta_t}s. Proof time: {proof_time}s. Overhead time: {delta_t - proof_time}s."
+    print(
+        f"Total response time {delta_t}s. Proof time: {proof_time}s. "
+        f"Overhead time: {delta_t - proof_time}s."
     )
-    if delta_t > 300:
-        bt.logging.error(
-            "Response time is greater than validator timeout. This indicates your hardware is not processing validator's requests in time."
-        )
     return synapse
-
-@app.get("/status")
-def get_status():
-    return True
 
 if __name__ == '__main__':
     import uvicorn
