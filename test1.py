7f3ef62294a9197ee5f0fb00ae09b5bffd015795
root <root@56070.example.us>
2024-07-01 12:38:40 -0400

    test1

--- test1.py
+++ test1.py
@@ -6,5 +6,6 @@ synapse = worker.QueryZkProof(
 	query_input = {'model_id': [0], 'public_inputs': [-0.2847691892031461, -0.08110789393296791, 0.6423077381281963, -0.19719430897525503, 0.1303140519756325]}
 )
 
-asyncio.run(worker.generateZkProof(synapse))
+result = asyncio.run(worker.generateZkProof(synapse))
 
+print(result)
