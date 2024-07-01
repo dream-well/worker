90288be781fea5ba0e8c5ef44c587304bb3ed3c9
root <root@56070.example.us>
2024-07-01 14:33:56 -0400

    fix test

--- test.py
+++ test.py
@@ -11,11 +11,12 @@ import time
 # print(result)
 retry = 0
 start = time.time()
-while retry < 10:
+while retry < 1:
 	synapse = worker.QueryZkProof(
 		query_input = {'model_id': ['0a92bc32ea02abe54159da70aeb541d52c3cba27c8708669eda634e096a86f8b'], 'public_inputs': [[0.00390625], [3.655120544458697e-17], [False], [5280], [10.941974639892578], [60.0], [11.105813026428223], [55.0, 52.0, 100.0, 97.0, 56.0, 55.0, 55.0, 48.0, 57.0, 98.0, 55.0, 98.0, 51.0, 99.0, 101.0, 101.0, 102.0, 54.0, 97.0, 57.0, 97.0, 99.0, 100.0, 101.0, 49.0, 57.0, 49.0, 54.0, 50.0, 55.0, 48.0, 97.0, 100.0, 54.0, 98.0, 52.0, 56.0, 53.0, 57.0, 52.0, 102.0, 101.0, 99.0, 100.0, 100.0, 52.0, 54.0, 100.0, 98.0, 101.0, 54.0, 56.0, 57.0, 57.0, 99.0, 54.0, 56.0, 97.0, 100.0, 49.0, 100.0, 52.0, 53.0, 56.0], [3216754], [208]]}
 	)
 
-	asyncio.run(worker.generateZkProof(synapse))
+	result = asyncio.run(worker.generateZkProof(synapse))
+	print(result)
 	retry += 1
 print("Time: ", (time.time() - start) / 10)
